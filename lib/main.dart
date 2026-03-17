623e74b0bf5ed4aee60d82d6a82675a9d5f5b6d1
potatoed6 <tv1k.fake@gmail.com>
2026-03-17 19:06:08 +0100

    lab03-2

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'KrakFlow'),
     );
   }
 }
@@ -104,7 +104,16 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            Center(
+              child: Column(
+                children:
+                [
+                  const Text('KrakFlow'),
+                  const Text('Organizacja studiów'),
+                  const Text('Dzisiejsze zadania'),
+                ],
+              )
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
